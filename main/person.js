647c90a622bbbaf301d1e9b4a60126763fd3a6f1
chuyingqifei <1425861237@qq.com>
2017-05-27 18:08:35 +0800

    first commit;

--- main/person.js
+++ main/person.js
@@ -1,2 +1,14 @@
 // Write your code here
 module.exports = Person
+class Person{
+	
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	introduce(){
+		//My name is Tom. I am 21 years old.
+		return "My name is ${this.name}.I am ${this.age} years old."
+	}
+}
+
--- main/student.js
+++ main/student.js
@@ -1,3 +1,15 @@
 let Person = require('./person')
 // Write your code here
 module.exports = Student
+
+class Student extends Person{
+	constructor(name,age,klass){
+		super(name,age);
+		this.klass = klass;
+	}
+	introduce(){
+		//My name is Tom. I am 21 years old. I am a Student. I am at Class 2.;
+		return "My name is ${this.name}. I am ${this.age} years old. I am a Student. I am at Class ${this.klass}.";
+	}
+	
+}
