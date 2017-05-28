69073774595da9f86615f13cf18c3707a0d08979
chuyingqifei <1425861237@qq.com>
2017-05-28 22:58:47 +0800

    first commit;

--- main/student.js
+++ main/student.js
@@ -25,6 +25,6 @@ Student.prototype.constructor = Student;
 
 Student.prototype.introduce = function(){
 	//return "My name is "+ this.name+"."+"I am " + this.age+" years old."+"I am a Student. I am at Class"+this.klass+".";
-	return "My name is "+ Student.name+"."+"I am " + Student.age+" years old."+"I am a Student. I am at Class"+this.klass+".";
+	return "My name is "+ this.name+". "+"I am " + this.age+" years old. "+"I am a Student. I am at Class"+this.klass+".";
 }
 
